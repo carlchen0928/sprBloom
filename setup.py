1a30933abb163064f523e0de5c428e2a84574178
cyy5121212 <515822895@qq.com>
2014-09-22 09:25:43 +0800

    compile with Cython

---
+++ setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+from distutils.core import setup
+from setuptools import setup
+
+ext_files = ['sprBloom/bloom.c']
+
+kwargs = {}
+
+try:
+    from Cython.Distutils import build_ext
+    from Cython.distutils import Extension
+    print 'Building from Cython'
+    ext_files.append('sprBloom/sprBloom.pyx')
+    kwargs['cmdclass'] = {'build_ext': build_ext}
+except ImportError:
+    from distutils.core import Extension
+    ext_files.append('sprBloom/sprBloom.c')
+    print 'Building from C'
+
+
+ext_modules = [Extension('sprBloom', ext_files, libraries=['hiredis'])]
+
+
+setup(
+    name = "spybloom",
+    version = '1.0.0',
+    author="Yiyu Chen",
+    author_email="chen_yiyu@foxmail.com",
+    license="MIT License",
+    ext_modules = ext_modules,
+    classifiers = [
+        'Intended Audience :: Developers', 
+        'License :: OSI Approved :: MIT License', 
+        'Programming Language :: C', 
+        'Programming Language :: Cython', 
+        'Topic :: Sofeware Development :: Libraries :: Python Modules',
+    ],
+    **kwargs
+)
--- sprBloom/sprBloom.pyx
+++ sprBloom/sprBloom.pyx
@@ -26,8 +26,8 @@ cimport bloom
 
 
 class pyreBloomException(Exception):
-	'''Some sort of exception has happened internally'''
-	pass
+    '''Some sort of exception has happened internally'''
+    pass
 
 class sprBloom(object):
     SMALL_SET_GROWTH = 2    #capacity increase by double
@@ -39,7 +39,7 @@ class sprBloom(object):
         self._setup(key, init_capacity, error, mode, host, port, password)
         self.filters = []
 
-    def _setup(key, init_capacity, error, mode, host, port, password):
+    def _setup(self, key, init_capacity, error, mode, host, port, password):
         self.key = key
         self.init_capacity = init_capacity
         self.error = error
@@ -95,69 +95,69 @@ class sprBloom(object):
         return sum(f.count for f in self.filters)
 
 cdef class pyreBloom(object):
-	cdef bloom.pyrebloomctxt context
-	cdef bytes               key
-	
-	property bits:
-		def __get__(self):
-			return self.context.bits
-	
-	property hashes:
-		def __get__(self):
-			return self.context.hashes
-	
-	def __cinit__(self, key, capacity, error, host='127.0.0.1', port=6379,
-		password=''):
-		self.key = key
+    cdef bloom.pyrebloomctxt context
+    cdef bytes               key
+    
+    property bits:
+        def __get__(self):
+            return self.context.bits
+    
+    property hashes:
+        def __get__(self):
+            return self.context.hashes
+    
+    def __cinit__(self, key, capacity, error, host='127.0.0.1', port=6379,
+        password=''):
+        self.key = key
         self.count = 0
         self.capacity = capacity
-		if bloom.init_pyrebloom(&self.context, self.key, capacity,
-			error, host, port, password):
-			raise pyreBloomException(self.context.ctxt.errstr)
-	
-	def __dealloc__(self):
-		bloom.free_pyrebloom(&self.context) 
-	
-	def delete(self):
-		bloom.delete(&self.context)
-	
-	def put(self, value):
-		if getattr(value, '__iter__', False):
-			r = [bloom.add(&self.context, v, len(v)) for v in value]
-			r = bloom.add_complete(&self.context, len(value))
+        if bloom.init_pyrebloom(&self.context, self.key, capacity,
+            error, host, port, password):
+            raise pyreBloomException(self.context.ctxt.errstr)
+    
+    def __dealloc__(self):
+        bloom.free_pyrebloom(&self.context) 
+    
+    def delete(self):
+        bloom.delete(&self.context)
+    
+    def put(self, value):
+        if getattr(value, '__iter__', False):
+            r = [bloom.add(&self.context, v, len(v)) for v in value]
+            r = bloom.add_complete(&self.context, len(value))
             self.count = self.count + len(value)
-		else:
-			bloom.add(&self.context, value, len(value))
-			r = bloom.add_complete(&self.context, 1)
+        else:
+            bloom.add(&self.context, value, len(value))
+            r = bloom.add_complete(&self.context, 1)
             self.count = self.count + 1
-		if r < 0:
-			raise pyreBloomException(self.context.ctxt.errstr)
-		return r
-	
-	def add(self, value):
-		return self.put(value)
-	
-	def extend(self, values):
-		return self.put(values)
-	
-	def contains(self, value):
-		# If the object is 'iterable'...
-		if getattr(value, '__iter__', False):
-			r = [bloom.check(&self.context, v, len(v)) for v in value]
-			r = [bloom.check_next(&self.context) for i in range(len(value))]
-			if (min(r) < 0):
-				raise pyreBloomException(self.context.ctxt.errstr)
-			return [v for v, included in zip(value, r) if included]
-		else:
-			bloom.check(&self.context, value, len(value))
-			r = bloom.check_next(&self.context)
-			if (r < 0):
-				raise pyreBloomException(self.context.ctxt.errstr)
-			return bool(r)
-	
-	def __contains__(self, value):
-		return self.contains(value)
+        if r < 0:
+            raise pyreBloomException(self.context.ctxt.errstr)
+        return r
+    
+    def add(self, value):
+        return self.put(value)
+    
+    def extend(self, values):
+        return self.put(values)
+    
+    def contains(self, value):
+        # If the object is 'iterable'...
+        if getattr(value, '__iter__', False):
+            r = [bloom.check(&self.context, v, len(v)) for v in value]
+            r = [bloom.check_next(&self.context) for i in range(len(value))]
+            if (min(r) < 0):
+                raise pyreBloomException(self.context.ctxt.errstr)
+            return [v for v, included in zip(value, r) if included]
+        else:
+            bloom.check(&self.context, value, len(value))
+            r = bloom.check_next(&self.context)
+            if (r < 0):
+                raise pyreBloomException(self.context.ctxt.errstr)
+            return bool(r)
+    
+    def __contains__(self, value):
+        return self.contains(value)
 
-	def keys(self):
-		'''Return a list of the keys used in this bloom filter'''
-		return [self.context.keys[i] for i in range(self.context.num_keys)]
+    def keys(self):
+        '''Return a list of the keys used in this bloom filter'''
+        return [self.context.keys[i] for i in range(self.context.num_keys)]
